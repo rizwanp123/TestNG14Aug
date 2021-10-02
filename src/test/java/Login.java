844e67a40f4b4509af61f3a0813b85f532aac8ed
rizwanp123 <md.rizwan960@gmail.com>
2021-10-02 14:18:53 +0530

    Add groups, parameters, listeners

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import driver.BaseClass;
@@ -18,10 +21,11 @@ public class Login extends BaseClass {
 		 driver.get("https://demoqa.com/login");
 	}
 	
-  @Test (priority = 3)
-  public void PositiveLogin() {
-	  driver.findElement(By.id("userName")).sendKeys("Rizwan@14aug");
-	  driver.findElement(By.id("password")).sendKeys("Rizwan@14aug");
+  @Test (priority = 3, groups= {"Login"})
+  @Parameters({"Username","Password"})
+  public void PositiveLogin(String username, String password) {
+	  driver.findElement(By.id("userName")).sendKeys(username);
+	  driver.findElement(By.id("password")).sendKeys(password);
 	  JavascriptExecutor js = (JavascriptExecutor) driver;
 	  js.executeScript("window.scrollBy(0,350)", "");
 	  
@@ -37,7 +41,7 @@ public class Login extends BaseClass {
   
   
   // TestNG methods are run in alphabetical order
-  @Test (priority = 4)
+  @Test (priority = 4,groups= {"Login","ignore"})
   public void NegativeLogin() {
 	  driver.findElement(By.id("userName")).sendKeys("Rizwan@14aug");
 	  driver.findElement(By.id("password")).sendKeys("Rizwan");
@@ -49,8 +53,13 @@ public class Login extends BaseClass {
 	  
 	  new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.id("name")));
 	  String errorMessage = driver.findElement(By.id("name")).getText();
-	  System.out.println("home page text is: " + errorMessage);
+	  System.out.println("Error text is: " + errorMessage);
 	  Assert.assertEquals(errorMessage,"Invalid username or password!", "Negative login assertion failed" );
 
+  }
+  
+  @AfterSuite(alwaysRun = true)
+  public void closeBrowser() {
+	  driver.quit();
   }
 }
